eaacb6f0dd3b75919a12a7b6ff69dbd0aa89b87e
Mark Mavis <mmavis@uw.edu>
2022-04-11 01:14:42 -0700

    Add IndirectRecursion Feature

--- Data Structures/Recursion/IndirectRecursion.cpp
+++ Data Structures/Recursion/IndirectRecursion.cpp
@@ -1,1 +1,16 @@
-#pragma once
+#include "IndirectRecursion.h"
+#include <iostream>
+
+void FunctionA(int n) {
+	if (n > 0) {
+		printf("%d", n);
+		FunctionB(n - 1);
+	}
+}
+
+void FunctionB(int n) {
+	if (n > 1) {
+		printf("%d", n);
+		FunctionA(n / 2);
+	}
+}
