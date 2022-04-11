7bcd25ec67882762d1a49cd6994fe4e8dcd06ea2
Mark Mavis <mmavis@uw.edu>
2022-04-10 23:57:19 -0700

    Add TreeRecursion Function

--- Data Structures/Recursion/TreeRecursion.h
+++ Data Structures/Recursion/TreeRecursion.h
@@ -1,5 +1,18 @@
 #pragma once
-class TreeRecursion
-{
-};
+#ifndef TREERECURSION_H
+#define TREERECURSION_H
+
+/*
+* TREE RECURSION
+* 
+* Tree recursion is defined as a function that calls itself more than once
+* 
+*/
+
+void TreeRecursion(int n);
+
+
+#endif
+
+
 
