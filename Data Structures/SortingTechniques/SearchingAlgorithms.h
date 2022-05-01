9006ebebcb0fcdd467d069b212694173180d473f
Mark Mavis <mmavis@uw.edu>
2022-05-01 14:35:29 -0700

    Add Sort Algo Project Files

--- Data Structures/SortingTechniques/SearchingAlgorithms.h
+++ Data Structures/SortingTechniques/SearchingAlgorithms.h
@@ -12,15 +12,3 @@ void ArrayCreator();
 
 
 
-void BubbleSort();
-void InsertionSort();
-void SelectionSort();
-void HeapSort();
-void MergeSort();
-void QuickSort();
-void Quicksort();
-void TreeSort();
-void ShellSort();
-void CountSort();
-void BucketSort();
-void RadixSort();
---
+++ Data Structures/SortingTechniques/SortingAlgorithms.h
@@ -0,0 +1,16 @@
+#pragma once
+
+void BubbleSort();
+void InsertionSort();
+void SelectionSort();
+void HeapSort();
+void MergeSort();
+void QuickSort();
+void Quicksort();
+void TreeSort();
+void ShellSort();
+void CountSort();
+void BucketSort();
+void RadixSort();
+
+void SortController();
